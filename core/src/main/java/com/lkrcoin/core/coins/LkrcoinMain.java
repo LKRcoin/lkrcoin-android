42d5a86997603e1761dbca809798f7a4fcd30944
Jose Luis Estevez <jose.estevez.prieto@gmail.com>
2018-12-23 21:57:10 +0100

    Change signedMessageHeader

--- core/src/main/java/com/lkrcoin/core/coins/LkrcoinMain.java
+++ core/src/main/java/com/lkrcoin/core/coins/LkrcoinMain.java
@@ -15,7 +15,7 @@ public class LkrcoinMain extends PeerFamily {
         spendableCoinbaseDepth = 500;
         dumpedPrivateKeyHeader = 128 + addressHeader;
 
-        name = "Lkrcoin";
+        name = "LKRcoin";
         symbol = "LKR";
         uriScheme = "lkrcoin";
         bip44Index = 557;  // Agregado en https://github.com/satoshilabs/slips/blob/master/slip-0044.md
@@ -24,7 +24,7 @@ public class LkrcoinMain extends PeerFamily {
         minNonDust = value(1);
         softDustLimit = value(1000000);
         softDustPolicy = SoftDustPolicy.AT_LEAST_BASE_FEE_IF_SOFT_DUST_TXO_PRESENT;
-        signedMessageHeader = toBytes("LakrassCoin Signed Message:\n");
+        signedMessageHeader = toBytes("LKRcoin Signed Message:\n"); // Cambio en el core https://github.com/LKRcoin/lkrcoin/commit/f4135e8cc5d8fd623c4238838b5012b1b70eed65
     }
 
     private static LkrcoinMain instance = new LkrcoinMain();
